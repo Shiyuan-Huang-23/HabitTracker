11269db91e0c86f13abd43b3031814b3caa7105e
Shiyuan Huang <shiyuancn@live.cn>
2020-02-02 23:55:41 -0500

    Add habit-tracking feature via calendar
    Currently cannot change color of entire date, only the event color.

--- app/src/main/java/com/example/habittracker/MainActivity.kt
+++ app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -2,22 +2,32 @@ package com.example.habittracker
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableStringBuilder
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.github.sundeepk.compactcalendarview.CompactCalendarView
+import com.github.sundeepk.compactcalendarview.domain.Event
 import org.json.JSONArray
 import org.json.JSONObject
+import java.text.ParsePosition
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
 
     private val useBackend = true
-    private val backendURL = ""
+    private val backendURL = "YOUR BACKEND URL"
 
+    private val dateFormat = SimpleDateFormat("MM/dd/yy")
     /** Invariant: There are no duplicates in habitList */
     private var habitList : ArrayList<String> = ArrayList()
     private var habitMap : HashMap<String, Int> = HashMap()
@@ -134,9 +144,7 @@ class MainActivity : AppCompatActivity() {
             PostJSONTask().execute(backendURL, "api/habit/$id/done/")
         }
 
-        val index = habitList.indexOf(completedHabit)
-        habitList.removeAt(index)
-        viewAdapter.notifyItemRemoved(index)
+        removeHabitFromView(completedHabit)
         Toast.makeText(applicationContext, "Habit completed!", Toast.LENGTH_SHORT).show()
         if (view is Button) {
             dialog.dismiss()
@@ -144,10 +152,48 @@ class MainActivity : AppCompatActivity() {
     }
 
     /**
+     * Effect: Marks a habit as skipped.
+     */
+    fun skipHabit(view: View) {
+        val parent = view.parent as LinearLayout
+        val habit = parent.findViewById<TextView>(R.id.habitTextView).text.toString()
+
+        if (useBackend) {
+            val id = habitMap[habit]
+            val postJson = JSONObject()
+            postJson.put("category", "skip")
+            postJson.put("skip_note", parent.findViewById<EditText>(R.id.notesEditText).text)
+            PostJSONTask().execute(backendURL, "api/event/$id/", postJson.toString())
+        }
+        removeHabitFromView(habit)
+        dialog.dismiss()
+    }
+
+    /**
+     * Effect: Removes a habit from the list of currently displayed habits.
+     */
+    private fun removeHabitFromView(habit: String) {
+        val index = habitList.indexOf(habit)
+        habitList.removeAt(index)
+        viewAdapter.notifyItemRemoved(index)
+    }
+
+    /**
      * Effect: Displays the Create Habit dialog
      */
     fun displayCreateHabitDialog(view: View) {
         dialog.setContentView(R.layout.create_habit)
+        dialog.show()
+    }
+
+    /**
+     * Effect: Displays the Skip Habit dialog
+     */
+    fun displaySkipHabitDialog(view: View) {
+        dialog.setContentView(R.layout.skip_habit)
+        val habit = ((view.parent as LinearLayout).parent as LinearLayout).findViewById<TextView>(R.id.habitTitleTextView).text.toString()
+        val habitTextView = dialog.findViewById<TextView>(R.id.habitTextView)
+        habitTextView.text = habit
         dialog.show()
     }
 
@@ -161,16 +207,42 @@ class MainActivity : AppCompatActivity() {
         val habit = (((view as Button).parent) as LinearLayout).findViewById<CheckBox>(R.id.checkBox).text
         habitTitleTextView.text = habit
         val notesTextView = dialog.findViewById<TextView>(R.id.notesTextView)
+        val calendar = dialog.findViewById<CompactCalendarView>(R.id.calendarView)
+        calendar.setFirstDayOfWeek(Calendar.SUNDAY)
         if (useBackend) {
             val id = habitMap[habit]
             val jsonStr = GetJSONTask().execute(backendURL, "api/habit/$id/").get()
             val json = JSONObject(jsonStr)
 
             notesTextView.text = json.getJSONObject("data").getString("notes")
+
+            val habitEvents = GetJSONTask().execute(backendURL, "api/events/$id/").get()
+            Log.i("JSON", habitEvents)
+
+            val eventJson = JSONArray(JSONObject(habitEvents).getString("data"))
+            val debugging = JSONObject(habitEvents).getString("data")
+            Log.i("JSON", debugging)
+            Log.i("Length", eventJson.length().toString())
+
+            val parsePos = ParsePosition(0)
+            for (i in 0 until eventJson.length()) {
+                val eventObj = eventJson.getJSONObject(i)
+                var color = Color.GREEN
+                val category = eventObj.getString("category")
+                if (category == "skip") {
+                    color = Color.RED
+                }
+                Log.i("date", eventObj.getString("date"))
+                val date = dateFormat.parse(eventObj.getString("date"), parsePos)
+                val epochTime: Long = date.time + 18000000
+
+                val event = Event(color, epochTime, eventObj.getString("skip_note"))
+                calendar.addEvent(event)
+            }
+
         } else {
             notesTextView.text = notesMap[habit]
         }
-
         dialog.show()
     }
 
